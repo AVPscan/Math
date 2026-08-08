d4273bdfc274adc1ec307b5c2357ea2357d1242b
Alexey Pozdnyakov <avp70ru@mail.ru>
2026-08-08 09:45:57 +0700

    Opt+

--- main.c
+++ main.c
@@ -11,16 +11,16 @@
 #include "sys.h"
 
 #define BE  0
-#define Lon 2
+#define Lon 1
 
 #if Lon == 1
  typedef uint8_t buf; typedef int8_t zbuf;
  typedef uint16_t Dbuf; typedef int16_t zDbuf;
- sanu a = {253}, b = {8};
+ sanu a = {0}, b = {128};
 #elif Lon == 2
  typedef uint16_t buf; typedef int16_t zbuf;
  typedef uint32_t Dbuf; typedef int32_t zDbuf;
- vanu a = {128,{1}}, b = {0,{8}};
+ vanu a = {129,{1}}, b = {2,{0}};
 #elif Lon == 4
  typedef uint32_t buf; typedef int32_t zbuf;
  typedef uint64_t Dbuf; typedef int64_t zDbuf;
@@ -38,14 +38,14 @@ void Show(anu i, const char *str, void *r, void *e) {
   if (Mat.Nim) { printf("%d %s %d = ", o3, str, o4); if (i > 1) printf("%d", *(zDbuf*)r); else printf("%d", *(zbuf*)r); }
   else { printf("%d %s %d = ", o1, str, o2); if (i > 1) printf("%d", *(Dbuf*)r); else printf("%d", *(buf*)r); }
 #endif
-  if (str[0] == '*') { printf(" long %d", Mat.Ll); }
-  else if (str[0] == '/') { if (Mat.Nim) printf(" tile %d", *(zbuf*)e); else printf(" tile %d", *(buf*)e); }
-  printf("\n"); }
+  if (str[0] == '*') { printf(" long %d", ((Mat.Lh << 8) | Mat.Ll)); }
+  else if (str[0] == '/') { if (Mat.Nim) printf(" tile %d", *(zbuf*)e); else printf(" tile %d", *(buf*)e); 
+    printf(" %s", (Mat.Eiz) ? (Mat.Enim) ? " I" : " Z" : (Mat.Enim) ? " -" : " +"); } printf("\n"); }
 
-int main(void) { buf r, e; Dbuf rm; zbuf zr, ze; zDbuf zm; Mat.L = Lon; Mat.Nim = 0; Mat.A = (anu*)&a.h; Mat.B = (anu*)&b.h;
+int main(void) { buf r, e; Dbuf rm; zbuf zr, ze; zDbuf zm; Mat.L = Lon; Mat.A = (anu*)&a.h; Mat.B = (anu*)&b.h;
   FSWAP(Mat.L, (anu*)&o1, Mat.A); FSWAP(Mat.L, (anu*)&o2, Mat.B); FSWAP(Mat.L, (anu*)&o3, (anu*)&a.h); FSWAP(Mat.L, (anu*)&o4, (anu*)&b.h);
   if (BE) { FSWAP(Mat.L, (anu*)&o1, (anu*)&o1); FSWAP(Mat.L, (anu*)&o2, (anu*)&o2); FSWAP(Mat.L, (anu*)&o3, (anu*)&o3); 
-    FSWAP(Mat.L, (anu*)&o4, (anu*)&o4); } printf("Long = %d\n", Lon); FADD(Mat.L, (anu*)&r, Mat.A, Mat.B); Show(1, "+", &r, &e);
+    FSWAP(Mat.L, (anu*)&o4, (anu*)&o4); } printf("Long = %d\n", Lon); Mat.Nim = 0; FADD(Mat.L, (anu*)&r, Mat.A, Mat.B); Show(1, "+", &r, &e);
   FSUB(Mat.L, (anu*)&r, Mat.A, Mat.B); Show(1, "-", &r, &e); FMUL(Mat.L, (anu*)&rm, Mat.A, Mat.B); Show(2, "*", &rm, &e); 
   FDIV(Mat.L, (anu*)&r, Mat.A, Mat.B, (anu*)&e); Show(1, "/", &r, &e); Mat.Nim = 1;
   FADD(Mat.L, (anu*)&zr, Mat.A, Mat.B); Show(1, "+", &zr, &ze); FSUB(Mat.L, (anu*)&zr, Mat.A, Mat.B); Show(1, "-", &zr, &ze);
