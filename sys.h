6174ca95394480d388f5181b4fa56d6b5081ac71
AVPscan <avp70ru@mail.ru>
2026-08-16 05:37:17 +0700

    Opt+++

--- sys.h
+++ sys.h
@@ -39,7 +39,7 @@ typedef struct { anu l, m[6], h; } van;                         // 8  v    [0..F
 //typedef struct { anu l, m[6]; nanu h; } vnan;                 // 8 vn    [0,+1..+7FFFFFFFFFFFFFFF,8000000000000000,-7FFFFFFFFFFFFFFF..-1]
 //typedef struct { anu l, m[1..253], h; };                      // 1-255        + весь диапазон доступен [1..2040] 10^614
 //typedef struct { anu l, m[1..253]; nanu h; };                 // 1-255n
-typedef struct { anu l, m[254], h; } MatBuf;                    // 256				  + бит	[2041..2048] 10^616 v10^1228
+typedef struct { anu l, m[254], h; } MatBuf;                    // 256                             + бит [2041..2048] 10^616 v{10^1228}
 typedef struct { union {anu l; anu m[1]; anu h;}; } Sanu;       // 1       возможность работать с anu как со структурой
 typedef struct { MatBuf Ho, Sr, Lo; anu Nim, Over, C, F, N, Fre, Nre,
   lb, lar, clar, tl, fa, fb, na, nb, dr, da, db, *r, *a, *b, *re, *R, *A, *B, *RE; } Cache; extern Cache Mat;
