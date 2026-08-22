2e949f07933d160e663c2136193de0d787544ee1
AVPscan <avp70ru@mail.ru>
2026-08-22 07:03:58 +0700

    v4.1

--- main.c
+++ main.c
@@ -10,25 +10,23 @@
 
 anu cl = 0, l = 1, a[] = {251,1,0,0}, lb, b[] = {6,0,0,0}, c[4], d[4], r[8], e[4];
 
-void Num( anu s, anu i, an a) {
-  if (Mat.Nim) { 
-    if (i) { if (s == 3) printf("%ld", *(int64_t*)a); else if (s == 1) printf("%d", *(int32_t*)a); else if (!s) printf("%d", *(int16_t*)a); }
-    else { if (s == 3) printf("%d", *(int32_t*)a); else if (s == 1) printf("%d", *(int16_t*)a); else if (!s) printf("%d", *(int8_t*)a); } }
-  else {
-    if (i) { if (s == 3) printf("%ld", *(uint64_t*)a); else if (s == 1) printf("%d", *(uint32_t*)a); else if (!s) printf("%d", *(uint16_t*)a); }
-    else { if (s == 3) printf("%d", *(uint32_t*)a); else if (s == 1) printf("%d", *(uint16_t*)a); else if (!s) printf("%d", *(uint8_t*)a); } } }
+void Num( anu s, an a) {
+  if (Mat.Nim) { if (s > 8) printf("XxX"); else if (s > 3) printf("%ld", *(int64_t*)a);
+    else if (s > 1) printf("%d", *(int32_t*)a); else if (s) printf("%d", *(int16_t*)a); else printf("%d", *(int8_t*)a); }
+  else { if (s > 8) printf("XxX"); else if (s > 3) printf("%ld", *(uint64_t*)a); else if (s > 1) printf("%d", *(uint32_t*)a); 
+    else if (s) printf("%d", *(uint16_t*)a); else printf("%d", *(uint8_t*)a); } }
 
 void Show(const char *s) { anu i = (s[0] == '*');
   printf(" %s%s", (Mat.C) ? "C" : " ", (Mat.F) ? (Mat.N) ? "I " : "Z " : (Mat.N) ? "- " : "+ "); Mat.C = 0;
   if (Mat.Be) { FSWAP(((Mat.lb + 1) * (1 + i)) - 1, (an)r, (an)r); FSWAP(Mat.lb, (an)e, (an)e); }
-  Num(Mat.lb, 0, (an)c); printf(" %s ", s); Num(Mat.lb, 0, (an)d); printf(" = "); Num(Mat.lb, i, (an)r);
-  if (s[0] == '*') { printf(" long %d", ((Mat.cl << 8) | Mat.l)); }
-  else if (s[0] == '/') { printf(" tile "); Num(Mat.lb, 0, (an)e);
-    printf(" %s", (Mat.Fre) ? (Mat.Nre) ? " I" : " Z" : (Mat.Nre) ? " -" : " +"); }
-  printf("\n"); }
+  Num(Mat.l, (an)c); printf(" %s ", s); Num(Mat.lb, (an)d); printf(" = ");
+  if (s[0] == '*') { Num(Mat.l, (an)r); printf(" long %d\n", ((Mat.cl << 8) | Mat.l)); }
+  else if (s[0] == '/') { Num(Mat.l, (an)r); printf(" tile "); Num(Mat.lb, (an)e);
+    printf(" %s", (Mat.Fre) ? (Mat.Nre) ? " I\n" : " Z\n" : (Mat.Nre) ? " -\n" : " +\n"); }
+  else { Num(Mat.l, (an)r); printf("\n"); } }
 
 int main(void) {
-  FINI(1,0,1); printf("long b %d\n", (lb = Mat.lb + 1)); Mat.A = (an)a; Mat.B = (an)b;
+  FINI(0,0,1); printf("long b %d\n", (lb = Mat.lb + 1)); Mat.A = (an)a; Mat.B = (an)b;
   FMOV(Mat.lb, (an)c, Mat.A); FMOV(Mat.lb, (an)d, Mat.B);
   if (Mat.Be) { FSWAP(Mat.lb, (an)c, (an)c); FSWAP(Mat.lb, (an)d, (an)d); }
   FADD(lb, r, Mat.A, Mat.B); Show("+"); Mat.cl = cl; Mat.l = l;
