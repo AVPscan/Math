9753749297881a0a5c76f2ce05cea04c95dc8f98
AVPscan <avp70ru@mail.ru>
2026-08-29 02:56:56 +0700

    v5.1

--- main.c
+++ main.c
@@ -12,27 +12,21 @@
 anu a[] = {255,0,0,0,0,0,0,0}, b[] = {255,254,0,0,0,0,0,0}, c[16], r[16], e[8];
 anu cl = 0, la = T, lb = T, Nim = 0, Be = 0, V = 0;
 
-void Num(anu s, an a) {
-  if (s > 7) { printf("XxX"); return; }
-  Fswap(s, (an)c, a); a = (an)c; FCold(s, a, a); s = Mat.lre;
+void Num(anu s, an a) { if (s > 7) { printf("XxX"); return; }
+  printf("(%d) ", s + 1); Fswap(s, (an)c, a); a = (an)c; FCold(s, a, a); s = Mat.lre;
   if (Mat.Nim) { if (s > 3) printf("%ld" , *(int64_t*)a); else if (s > 1) printf("%d ", *(int32_t*)a);
     else if (s) printf("%d ", *(int16_t*)a); else printf("%d ", *(int8_t*)a); }
   else { if (s > 3) printf("%ld ", *(uint64_t*)a); else if (s > 1) printf("%d ", *(uint32_t*)a);
     else if (s) printf("%d ", *(uint16_t*)a); else printf("%d ", *(uint8_t*)a); } }
 
-void Show(const char *s) {
-  printf("\n%s%s", (Mat.C) ? "C" : " ", (Mat.F) ? (Mat.N) ? "I " : "Z " : (Mat.N) ? "- " : "+ ");
-  printf("%s%s ", Mat.Nim ? "n" : " ", Mat.V ? "v " : "  "); printf("(%d %d %s %d) = ", la + 1, lb, s, Mat.l + 1); Num(Mat.l, Mat.R);
-  if (s[0] == '/') { printf("(%d) %s tile ", lb, (Mat.Fre) ? (Mat.Nre) ? "I" : "Z" : (Mat.Nre) ? "-" : "+"); Num(Mat.lb, Mat.RE); } 
-  Mat.C = 0; Flong(cl, la); }
+void Show(char s) { anu i = 4; printf("\nA%cB\n", s); while(i--) { Mat.Nim = ((3 - i) & 2) ? 1 : 0; Mat.V = ((3 - i) & 1) ? 1 : 0;
+    if (s == 'a') Fadd(Mat.lb, Mat.R, Mat.A, Mat.B); else if (s == '+') FADD(Mat.lb + 1, Mat.R, Mat.A, Mat.B);
+    else if (s == '-') FSUB(Mat.lb + 1, Mat.R, Mat.A, Mat.B); else if (s == '*') FMUL(Mat.lb + 1, Mat.R, Mat.A, Mat.B);
+    else if (s == '/') FDIV(Mat.lb + 1, Mat.R, Mat.A, Mat.B, Mat.RE);
+    printf("%c%c%c", (Mat.C) ? 'C' : ' ', (Mat.F) ? (Mat.N) ? 'I' : 'Z' : (Mat.N) ? '-' : '+', Mat.V ? 'v' : ' ');
+    printf("%c ", Mat.Nim ? 'n' : ' '); Num(Mat.l, Mat.R); if (!Mat.V) { Num(la, Mat.A); Num(Mat.lb, Mat.B); }
+    if (s == '/') { printf("%c tile ", (Mat.Fre) ? (Mat.Nre) ? 'I' : 'Z' : (Mat.Nre) ? '-' : '+'); Num(Mat.lb, Mat.RE); } 
+    printf("\n"); Mat.C = 0; Flong(cl, la); } }
 
-void Test(anu type) {
-  Mat.Nim = type; Fadd(Mat.lb, Mat.R, Mat.A, Mat.B); Show("add"); FADD(lb, Mat.R, Mat.A, Mat.B); Show("+"); 
-  FSUB(lb, Mat.R, Mat.A, Mat.B); Show("-"); FMUL(lb, Mat.R, Mat.A, Mat.B); Show("*");
-  FDIV(lb, Mat.R, Mat.A, Mat.B, Mat.RE); Show("/"); }
-
-int main(void) {
-  Fini(Nim, V, Be, cl, la, lb); lb = Mat.lb + 1; Mat.A = (an)a; Mat.B = (an)b; Mat.R = (an)r; Mat.RE = (an)e;
-  printf("A (%d) ", Mat.l + 1); Num(Mat.l, Mat.A); Mat.Nim++; Num(Mat.l, Mat.A); Mat.Nim--;
-  printf("\nB (%d) ", Mat.lb + 1); Num(Mat.lb, Mat.B); Mat.Nim++; Num(Mat.lb, Mat.B); Mat.Nim--;
-  Test(Nim); Test(~Nim); Fini(Nim, ~V, Be); Flong(cl, la, lb - 1); Test(Nim); Test(~Nim); return 0; }
+int main(void) { Fini(Nim, V, Be, cl, la, lb); Mat.A = (an)a; Mat.B = (an)b; Mat.R = (an)r; Mat.RE = (an)e;
+  Show('a'); Show('+'); Show('-'); Show('*'); Show('/'); return 0; }
