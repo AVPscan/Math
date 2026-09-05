8c1aa9e456ede585db6da2a1a0636e0dff50ff18
AVPscan <avp70ru@mail.ru>
2026-09-06 02:08:41 +0700

    v7.4

--- math.h
+++ math.h
@@ -27,35 +27,36 @@ typedef uintptr_t As;                           // Разрядность про
 typedef uint8_t anu;                            // Байт - атом
 typedef anu* an;                                // Начальный адрес расположения - число
 typedef struct { anu l, m[254], h, e; } MatBuf;	// 256 атомов + 1 сдвиг {умножение/деление}
-typedef struct { MatBuf Li, Hi; anu Nim, V, Be, lar, lbe, C, F, N, Fe, Ne, fa, na, fb, nb,
+typedef struct { MatBuf Li, Hi; anu Nim, V, Be, // Структура реализации библиотеки
+  lar, lbe, C, F, N, Fe, Ne, fa, na, fb, nb,
   dr, de, da, db; an r, e, a, b, R, E, A, B; } Cache;
 extern Cache Mat;
 
-void _FInit(anu x, anu y, an r, anu c, an a);               // Инициализация библиотеки
+void _FInit(anu x, anu y, an r, anu c, an a);   // Инициализация библиотеки
 void _FAddr (anu y, As* r, anu c, As* a);
-void FLD(an r, anu D);                                      // Создание числа из атома
+void FLD(an r, anu D);                          // Создание числа из атома
 #define Fld(D) FLD(Mat.R, D)
-void FLVD(an r, anu Dl, anu Dh);                            // Создание числа из двух атомов
+void FLVD(an r, anu Dl, anu Dh);                // Создание числа из двух атомов
 #define Flvd(Dl, Dh) FLVD(Mat.R, Dl, Dh)
-void FMOV(an r, an a, anu l);                               // Копирование числа
+void FMOV(an r, an a, anu l);                   // Копирование числа
 #define Fmov(l) FMOV(Mat.R, Mat.A, l)
-void FSWAP(an r, an a, anu l);                              // Зеркалирование атомов внутри относительно центра числа
+void FSWAP(an r, an a, anu l);                  // Зеркалирование атомов относительно центра числа
 #define Fswap(l) FSWAP(Mat.R, Mat.A, l)
-void FVIKARA(an r, an a, anu l);                            // Модификация длины числа
+void FVIKARA(an r, an a, anu l);                // Модификация длины числа
 #define Fvikara(l) FVIKARA(Mat.R, Mat.A, l)
-void FCOLD(an r, an a, anu l);                              // Приведение к старым форматам l = 1, 2, 4, 8, 16, 32, 64, 128, 256 байт 
+void FCOLD(an r, an a, anu l);                  // Приведение к формату l = 1,2,4,8,16,32,64,128,256 атомов
 #define Fcold(l) FCOLD(Mat.R, Mat.A, l)
-void FADD(an r, an a, anu l, an b);                         // Сложение r = a + b
+void FADD(an r, an a, anu l, an b);             // Сложение r = a + b
 #define Fadd(l, b) FADD(Mat.R, Mat.A, l, b)
-void FSUB(an r, an a, anu l, an b);                         // Вычитание r = a - b
+void FSUB(an r, an a, anu l, an b);             // Вычитание r = a - b
 #define Fsub(l, b) FSUB(Mat.R, Mat.A, l, b)
-void FMUL(an r, an a, anu l, an b);                         // Умножение r = a * b
+void FMUL(an r, an a, anu l, an b);             // Умножение r = a * b
 #define Fmul(l, b) FMUL(Mat.R, Mat.A, l, b)
-void FDIV(an r, an e, an a, anu l, an b);                   // Деление r = a / b, e = a mod b
+void FDIV(an r, an e, an a, anu l, an b);       // Деление r = a / b, e = a mod b - Евклид {положительный}
 #define Fdiv(l, b) FDIV(Mat.R, Mat.E, Mat.A, l, b)
 #define _anu(...) (anu)((sizeof((anu[]){0, ##__VA_ARGS__}) / sizeof(anu)) - 1), (anu[]){0, ##__VA_ARGS__} + 1
 #define _adr(...) (sizeof((As[]){0, ##__VA_ARGS__}) / sizeof(As)) - 1, (As[]){0, ##__VA_ARGS__} + 1
-#define Fini(...) _FInit(10,5,&Mat.Ne,_anu(__VA_ARGS__))    // Nim=V=Be=lb=la=cl=C=F=Fe=Ne=0, {Nim{,V{,Be{,lb{,la}}}}} (Mat.Nim = Nim; Mat.V = V; ..)
-#define Flong(...) _FInit(0,2,&Mat.Be,_anu(__VA_ARGS__))    // {lb{,la}} (Mat.lbe = lb; Mat.lar = la;)
-#define Faddr(...) _FAddr(4,(As*)&Mat.R,_adr(__VA_ARGS__))  // {r{,e{,a{,b}}}} (Mat.R = r; Mat.E = e; Mat.A = a; Mat.B = b;)
+#define Faddr(...) _FAddr(4,(As*)&Mat.R,_adr(__VA_ARGS__)) // {r{,e{,a{,b}}}} (Mat.R = r; ..)
+#define Fini(...) _FInit(10,5,&Mat.Ne,_anu(__VA_ARGS__))   // Nim=V=Be=..Ne=0 {Nim{,V{,Be{,la{,lb}}}}}
+#define Flong(...) _FInit(0,2,&Mat.Be,_anu(__VA_ARGS__))   // {la{,lb}} (Mat.lar = la; Mat.lbe = lb;)
 #endif
